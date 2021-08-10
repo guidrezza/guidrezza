3bb0c95a8eb7a5a9959cc793d634da56757044fc
guidrezza <38967582+guidrezza@users.noreply.github.com>
2021-08-10 16:31:21 -0500

    csc125

---
+++ lab1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Content-type: text/html\n\n";
+	
+	cout << "Hello Web!\n";
+	cout << "My name is Guilherme Busato.\n";
+	cout << "Goodbye Web!\n";
+}
